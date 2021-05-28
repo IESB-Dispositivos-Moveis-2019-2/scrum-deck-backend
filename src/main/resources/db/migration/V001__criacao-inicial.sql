a72ae201cbe892a2627c73a2515f1ff28f1b269b
Pedro Henrique Ferreira Figueiredo <pedro.figueiredo@castgroup.com.br>
2021-05-27 23:22:20 -0300

    Adicionada compatibilidade com PostgreSQL

--- src/main/resources/db/migration/V001__criacao-inicial.sql
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -1,30 +1,30 @@
 CREATE TABLE sprint (
-    id IDENTITY NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    link VARCHAR_IGNORECASE(255)
+    link VARCHAR(255)
 );
 
 CREATE TABLE estoria (
-    id IDENTITY NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     id_sprint BIGINT NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    link VARCHAR_IGNORECASE(255),
+    link VARCHAR(255),
     pontuacao INT,
 
     CONSTRAINT FK_SPRINT FOREIGN KEY (id_sprint) REFERENCES sprint (id)
 );
 
 CREATE TABLE desenvolvedor (
-    id IDENTITY NOT NULL PRIMARY KEY,
+    id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR_IGNORECASE(255) NOT NULL
+    email VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE voto (
     id_estoria BIGINT NOT NULL,
     id_desenvolvedor BIGINT NOT NULL,
     pontos INT NOT NULL,
-    data_hora DATETIME NOT NULL,
+    data_hora TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 
     CONSTRAINT PK_VOTO PRIMARY KEY (id_estoria, id_desenvolvedor),
     CONSTRAINT FK_ESTORIA FOREIGN KEY (id_estoria) REFERENCES estoria (id),
